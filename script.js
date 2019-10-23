ec5487fb2de0b63b8b8fee1b2bb84256afac13dd
Murad <murad-arslangiraev-1994@mail.ru>
2019-10-23 17:07:08 +0300

    уже который раз переделываю сайт, сверстал главную страницу

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.portfolio-site__blocks').not(':first').hide();
-    $('.portfolio-block__subtext .portfolio-subtext').click(function() {
-        $('.portfolio-block__subtext .portfolio-subtext').removeClass('subtext-active').eq($(this).index()).addClass('subtext-active');
-        $('.portfolio-site__blocks').hide().eq($(this).index()).fadeIn()
-    }).eq(0).addClass('subtext-active');
-
-});
